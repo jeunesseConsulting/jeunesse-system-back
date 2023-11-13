48b1b1b6daa2f75d2bd8fbdf58ff11d1f5f16e28
fanecovsf <gustavo_h07@hotmail.com>
2023-11-13 08:52:45 -0300

    🆕 Feat: Adicionado campo de status ao modelo de OS

--- backend/service_order/models.py
+++ backend/service_order/models.py
@@ -41,6 +41,7 @@ class ServiceOrder(models.Model):
 
     services_total_value = models.FloatField(default=0.0)
     products_total_value = models.FloatField(default=0.0)
+    status = models.CharField(max_length=255, default='Pendente')
 
     created_at = models.DateTimeField(auto_now_add=True)
 
