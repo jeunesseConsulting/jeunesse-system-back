aead86f394d5a2ed0dc2e78a04d0eb1b70a4cb63
fanecovsf <gustavo_h07@hotmail.com>
2023-11-14 11:53:05 -0300

    🆕 Feat: Implementado endpoint para geração do PDF através do json da os

--- backend/service_order/urls.py
+++ backend/service_order/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from service_order.views.service_order import ServiceOrderView, ServiceOrderDetailView, ServiceOrderPDFView, ServiceOrderReportView, ServiceOrderSummaryView
-
+from service_order.views.pdf import PDFGeneratorView
 
 urlpatterns = [
     path('', ServiceOrderView.as_view(), name='service_order_view'),
     path('/<id>', ServiceOrderDetailView.as_view(), name='service_order_detail_view'),
     path('/pdf/<id>', ServiceOrderPDFView.as_view(), name='service_order_pdf_view'),
+    path('/pdf-generator/base64', PDFGeneratorView.as_view(), name='pdf_generator_view'),
     path('/v1/report', ServiceOrderReportView.as_view(), name='service_order_report_view'),
     path('/v1/summary', ServiceOrderSummaryView.as_view(), name='service_order_summary_view'),
 ]
---
+++ backend/service_order/views/pdf.py
@@ -0,0 +1,22 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from service_order.utils import generate_service_order_pdf
+
+from backend.settings import CLIENT_NAME
+
+
+class PDFGeneratorView(APIView):
+
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        if request.data:
+            pdf64 = generate_service_order_pdf(request.data, CLIENT_NAME)
+            return Response(data={'pdf':pdf64}, status=status.HTTP_200_OK)
+        else:
+            return Response(data={'message':'invalid data'})
+
