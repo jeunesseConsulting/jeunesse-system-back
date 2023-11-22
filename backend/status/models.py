6c1fe0dcd62333cfba49af1df52d5821eb56c37b
fanecovsf <gustavo_h07@hotmail.com>
2023-11-21 22:37:02 -0300

    🐍 Wip: Implementando microserviço para cadastrar status padrão

--- backend/status/models.py
+++ backend/status/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Status(models.Model):
 
 
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
 
     def __str__(self):
         return self.name
