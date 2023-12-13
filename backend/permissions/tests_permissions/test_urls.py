1972b0a4099a7738a82ed061e3180b74775cfd4f
fanecovsf <gustavo_h07@hotmail.com>
2023-12-13 14:35:08 -0300

    🧪 Test: Testes implementados - product

--- backend/permissions/tests_permissions/test_urls.py
+++ backend/permissions/tests_permissions/test_urls.py
@@ -12,5 +12,5 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func.view_class, PermissionsView)
 
     def test_permissions_detail_url_is_resolved(self):
-        url = reverse('permissions_detail_view')
+        url = reverse('permissions_detail_view', kwargs={'id':1})
         self.assertEquals(resolve(url).func.view_class, PermissionsDetailView)
