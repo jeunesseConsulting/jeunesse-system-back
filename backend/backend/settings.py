f4a884d400a3d38aab5f2d68f921df6f8ef167c5
fanecovsf <gustavo_h07@hotmail.com>
2023-12-15 14:21:01 -0300

    Scret key hide

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d1m#^o)0w-#gyad6pk*qx_o$pnt46&c(s&c600-=d7--yq$%7)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if int(os.environ.get('DEBUG')) == 1:
