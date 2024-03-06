798a670ca4d542c26d1f52b849e30eb516994a87
fanecovsf <gustavo_h07@hotmail.com>
2024-03-06 14:28:03 -0300

    🆕 Feat: Adicionado job para criação do usuário de admin padrão na primeira inicialização

--- backend/status/apps.py
+++ backend/status/apps.py
@@ -13,7 +13,6 @@ class StatusConfig(AppConfig):
                 record_count = cursor.fetchone()[0]
             except:
                 record_count = 100
-                print("database doesn't exist")
                 pass
 
         if record_count < 7:
@@ -34,4 +33,19 @@ class StatusConfig(AppConfig):
                     Status.objects.get_or_create(name=obj)
                 except:
                     pass
+
+        # Criando usuário admin se o mesmo não existir
+        from user.services.user import UserService
+        try:
+            UserService.create(
+                name='admin',
+                last_name='admin',
+                email='admin@admin.com',
+                password='admin@123',
+                person_type='P',
+                document='123',
+                phone='123'
+            )
+        except:
+            pass
 
